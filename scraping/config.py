45c1f384d532781f43247384e666f5055ef871f2
Kaique Reis <kreis1265@gmail.com>
2026-03-08 20:20:47 -0300

    chore: add environment variables for db configuration

--- scraping/config.py
+++ scraping/config.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_URL = "https://webscraper.io/test-sites/e-commerce/allinone"
 DB_NAME = "products.db"
 MAX_PRODUCTS_CATEGORY = 100
@@ -11,4 +16,12 @@ COMMON_HEADERS = {
     "Accept-Language": "pt-BR,pt;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
+}
+
+DB_CONFIG = {
+    "host": os.getenv("POSTGRES_HOST", "localhost"),
+    "port": int(os.getenv("POSTGRES_PORT")),
+    "dbname": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
 }
